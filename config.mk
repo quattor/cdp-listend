7f8c44d6f46fbe7557bc75d6a35300c819f75023
brarda <brarda@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-11-14 16:07:55 +0000

    2011-11-14  Loic Brarda
    	* Release: 1.1.0
    	- Add configurable syslog facility parameter.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/cdp-listend@3023 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ COMP=cdp-listend
 
 NAME=$(COMP)
 DESCR=Configuration Distribution Protocol client deamon
-VERSION=1.0.20
+VERSION=1.1.0
 RELEASE=1
 
 AUTHOR=Piotr Poznanski
@@ -26,4 +26,4 @@ ifeq ($(QTTR_OS), Solaris)
   LOCKFILE=$(QTTR_LOCKD)/cdp-listend
 endif
   
-DATE=21/07/11 17:55
+DATE=14/11/11 17:07
